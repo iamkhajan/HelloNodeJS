1e45ad72dc08591ad934370d93751927e15f0285
khajanpandey <khajanpandey.hdw@gmail.com>
2020-03-28 12:39:18 -0600

    remving modules

--- HelloWorld.js
+++ HelloWorld.js
@@ -1,8 +1,9 @@
-var http = require('http');
-var firstModule = require('./myfirstmodule');
+// var http = require('http');
+// var firstModule = require('./myfirstmodule');
+console.log('Namaste Khajan')
 
-http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write("The date and time : " + firstModule.myDateTime()+"\n");
-    res.end('  Hello World!');
-}).listen(8080);
+// http.createServer(function (req, res) {
+//     res.writeHead(200, {'Content-Type': 'text/html'});
+//     res.write("The date and time : " + firstModule.myDateTime()+"\n");
+//     res.end('  Hello World!');
+// }).listen(8080);
